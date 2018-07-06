fa5047d67c1a56c9fc755c0536e32fea24fb8b0a
Austin Gomez <austingomez@hotmail.ca>
2018-07-06 00:24:23 -0400

    Playing around with triangle CSS. Looks terrible but was fun to implement

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Anime from 'react-anime';
-
+import headshot from '../../img/headshot.png'
+import tbj from '../../img/logo.png'
+import fr from '../../img/frlogo.png'
+import lflogo from '../../img/lf-logo2.png'
 import './Home.css';
 
 export default class Home extends Component {
     render() {
+        const companies = [
+            {
+                name: "Freshii",
+                logo: fr
+            },
+            {
+                name: "Toronto Blue Jays",
+                logo: tbj
+            },
+            {
+                name: "LyricFind",
+                logo: lflogo
+            }
+        ]
+
+        const listCompanies = companies.map((company, index) => {
+            return (
+                <div className="section" key={company.name+index}>
+                    <div className="full-height container">
+                        <h1>{company.name}</h1>
+                        <img className="circle-img" src={company.logo} alt="company logo"/>
+                    </div >
+                </div>
+            )
+        })
         return (
-            <div className="full-height container">
-                <Anime easing="easeInSine"
-                    duration={300}
-                    delay={(el, index) => index * 1500}
-                    opacity={[0, 1]}>
-                    <h2 className="Aligner-item">Hello</h2>
-                    <h2 className="Aligner-item">My name's Austin.</h2>
-                    <h2 className="Aligner-item">Lets make things together.</h2>
-                    <div className="links">
-                        <Link to="/">Another Link </Link>
-                        <Link to="/">Hello</Link>
+            <div className="Home">
+                <div className="top">
+                    <div className="top-container">
+                        <h1>Austin Gomez</h1>
+                        <h2>Web developer, designer, freelancer</h2>
                     </div>
-                </Anime>
-            </div >
+                    <img className="circle-img" src={headshot} alt="headshot"/>
+                </div>
+
+                <div className="section">
+                    <div className="full-height container">
+                        <h1>Work Experience</h1>
+                    </div >
+                </div>
+                {listCompanies}
+                <div className="top">
+                    <div className="top-container">
+                        <h1>Austin Gomez</h1>
+                        <h2>Web developer, designer, freelancer</h2>
+                    </div >
+                </div>
+            </div>
         )
     }
 }
