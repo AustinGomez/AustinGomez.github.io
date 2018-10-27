aecee5c4943ff658fdb4a431243f94eea1ccb44c
Austin <austin.gomez@freshii.com>
2018-10-27 13:27:00 -0400

    Updates

---
+++ precache-manifest.26e93eb23d3eb56c5728ed96582d7e45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5e99140bdba76b91243e431b63be7614",
+    "url": "/static/media/bluejays_logo.5e99140b.png"
+  },
+  {
+    "revision": "b2f8abb0e5360eeabd8deca229489e90",
+    "url": "/static/media/avatar-min.b2f8abb0.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e323f0764262954d0aa",
+    "url": "/static/js/main.5e323f07.chunk.js"
+  },
+  {
+    "revision": "cfa65833abaf6f08663e",
+    "url": "/static/js/1.cfa65833.chunk.js"
+  },
+  {
+    "revision": "5e323f0764262954d0aa",
+    "url": "/static/css/main.0719945b.chunk.css"
+  },
+  {
+    "revision": "da77e6483d18e6d3e0c92dfc32e6a2af",
+    "url": "/index.html"
+  }
+];
